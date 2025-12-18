d000b82e31e653bf36317c244ef906c693298deb
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-12-18 18:03:38 +0000

    feat(domain): create a VO to represent the statistics of a match

---
+++ crates/engine/src/domain/competitions/value_objects/match/statistics.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MatchStatistics {
+    pub home_goals: u8,
+    pub away_goals: u8,
+}
+
+impl MatchStatistics {
+    pub fn new() -> Self {
+        Self {
+            home_goals: 0,
+            away_goals: 0,
+        }
+    }
+
+    pub fn update(&mut self, home_goals: u8, away_goals: u8) {
+        self.home_goals = home_goals;
+        self.away_goals = away_goals;
+    }
+}
